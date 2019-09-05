83f5445317260e5e73b8e176d143caf34e3176ac
Matt Andes <andesm@objectcomputing.com>
2019-09-05 12:21:54 -0500

    Adding powershell equivelent of claimfunds.sh

---
+++ claimfunds.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$Account,
+    [Parameter(Mandatory)]
+    [string]$Key,
+    [string]$Type = "cn",
+    [string]$Endpoint = "pub-infra.opesx.io",
+    [switch]$Quiet
+)
+
+$ErrorActionPreference = "Stop"
+
+# Set Docker input variables
+$DOCKER_TAG = "latest"
+$CF_ACCOUNT = $Account
+$CF_ENDPOINT = $Endpoint
+$CF_KEY = $Key
+$CF_TYPE = $Type
+if ($Quiet) {
+    $CF_QUIET = "True"
+} Else {
+    $CF_QUIET = "False"
+}
+
+# Validate input
+if ($CF_TYPE -notmatch "^cn$|^an$|^in$") {
+    Write-Error "Invalid reward type: $CF_TYPE"
+}
+
+
+docker pull opespe/infranode:$DOCKER_TAG
+docker run --rm -it -v $PSScriptRoot:/src \
+    -e NODEOS_ADDR=$CF_ENDPOINT \
+    -e CF_ACCOUNT=$CF_ACCOUNT \
+    -e CF_KEY=$CF_KEY \
+    -e CF_TYPE=$CF_TYPE \
+    -e CF_QUIET=$CF_QUIET \
+    --entrypoint bash opespe/infranode:$DOCKER_TAG /src/lib/claimfunds.sh
