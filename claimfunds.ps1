ae97411af7087aa89b907cbc5fa6c596c723ab35
Matt Andes <andesm@objectcomputing.com>
2019-09-05 14:48:09 -0500

    Adding help text, adding prompt for required input if not provided, adding support for environment variables for script inputs.

--- claimfunds.ps1
+++ claimfunds.ps1
@@ -1,24 +1,75 @@
 param(
-    [Parameter(Mandatory)]
     [string]$Account,
-    [Parameter(Mandatory)]
     [string]$Key,
     [string]$Type = "cn",
     [string]$Endpoint = "pub-infra.opesx.io",
-    [switch]$Quiet
+    [switch]$Quiet,
+    [switch]$h
 )
 
 $ErrorActionPreference = "Stop"
 
+$usage = "A script for claiming reward funds.
+
+Usage:
+    claimfunds.ps1 [-h] -a|--account <account name> -k|--key <private key> [-t|--type <reward type>] [-e|--endpoint <api endpoint hostname>] [-q|--quiet]
+
+Where:
+    -h                Show this help text
+    -Account          (Required) - The block chain account you want to claim funds for.
+    -Key              (Required) - Private key of account claiming funds.
+    -Type             (Optional) - The type of rewards to claim. Options are cn, an, and in. Defaults to 'cn'.
+    -Endpoint         (Optional) - The Nodeos API endpoint to run commands against. Defaults to 'pub-infra.opesx.io'.
+    -Quiet            (Optional) - If provided, script does not prompt for confirmation before attempting to claim funds.
+
+Note:
+    You can use the following environment variables instead of providing command line options. Environment variables will override command line args if both are specified for a setting.
+        - CF_ACCOUNT
+        - CF_KEY
+        - CF_TYPE
+        - CF_ENDPOINT
+"
+
+if ($h) {
+    Write-Host $usage
+    exit 0
+}
+
 # Set Docker input variables
 $DOCKER_TAG = "latest"
-$CF_ACCOUNT = $Account
-$CF_ENDPOINT = $Endpoint
-$CF_KEY = $Key
-$CF_TYPE = $Type.tolower()
-if ($Quiet) {
+if ($env:CF_ACCOUNT) {
+    $CF_ACCOUNT = $env:CF_ACCOUNT
+} else {
+    if ($Account) {
+        $CF_ACCOUNT = $Account
+    } else {
+        $CF_ACCOUNT = Read-Host -Prompt "Block Chain Account Name"
+    }
+}
+if ($env:CF_KEY) {
+    $CF_KEY = $env:CF_KEY
+} else {
+    if ($Key) {
+        $CF_KEY = $Key
+    } else {
+        $EncryptedKey = Read-Host -Prompt "Account Private Key" -AsSecureString
+        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($EncryptedKey)
+        $CF_KEY = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    }
+}
+if ($env:CF_ENDPOINT) {
+    $CF_ENDPOINT = $env:CF_ENDPOINT
+} else {
+    $CF_ENDPOINT = $Endpoint
+}
+if ($env:CF_TYPE) {
+    $CF_TYPE = $env:CF_TYPE.tolower()
+} else {
+    $CF_TYPE = $Type.tolower()
+}
+if ($Quiet -or $env:CF_QUIET -eq "True") {
     $CF_QUIET = "True"
-} Else {
+} else {
     $CF_QUIET = "False"
 }
 
@@ -28,6 +79,5 @@ if ($CF_TYPE -notmatch "^cn$|^an$|^in$") {
 }
 
 $SCRIPTPATH = "/" + ${PSScriptRoot}.Split(":")[0].tolower() + ${PSScriptRoot}.Split(":")[1].Replace("\","/")
-echo $SCRIPTPATH
 docker pull opespe/infranode:$DOCKER_TAG
 docker run --rm -it -v "${SCRIPTPATH}:/src" -e NODEOS_ADDR=$CF_ENDPOINT -e CF_ACCOUNT=$CF_ACCOUNT -e CF_KEY=$CF_KEY -e CF_TYPE=$CF_TYPE -e CF_QUIET=$CF_QUIET --entrypoint bash opespe/infranode:$DOCKER_TAG /src/lib/claimfunds.sh
